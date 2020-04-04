5caad5d49c4df2a1ef23a79ead2be57e32d00e42
vijay narkademilli <vijaykumar.narkademilli@gmail.com>
2020-04-04 19:35:50 +0530

    schema creation script

---
+++ services/myCampus/src/main/java/com/mycampus/services/DBScripts/schema.sql
@@ -0,0 +1,9 @@
+CREATE USER mycampus IDENTIFIED BY Madhu$33;
+
+GRANT create session TO mycampus;
+GRANT create table TO mycampus;
+GRANT create view TO mycampus;
+GRANT create any trigger TO mycampus;
+GRANT create any procedure TO mycampus;
+GRANT create sequence TO mycampus;
+GRANT create synonym TO mycampus;
